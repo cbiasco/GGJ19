5c80cb7034bd114db2cf3c957eba369938399a8c
Caleb Biasco <biasc007@umn.edu>
2019-01-26 10:27:36 -0500

    PlayerController moves things

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    
+    public float speed = 5f;
+
+    private Rigidbody2D m_rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float vert = Input.GetAxis("Vertical");
+        float hor  = Input.GetAxis("Horizontal");
+
+        m_rigidBody.velocity = new Vector2(hor, vert) * speed;
     }
 }
