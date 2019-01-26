cbddbb8d9b78e850ea763942ecf118d311fb9a5a
Rajat Gupta <rajatg1@andrew.cmu.edu>
2019-01-26 14:54:26 -0800

    Added eraser script
    Add to the tilemap that needs erasing and then add the transform for the position that checks cell position

---
+++ Assets/Scripts/Eraser.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Eraser : MonoBehaviour
+{
+    public Transform player;
+    private Tilemap map;
+    private bool eraseable= false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && eraseable)
+        {
+            var tilePos = map.WorldToCell(player.transform.position);
+            if (map.GetTile(tilePos) !=null)
+                map.SetTile(tilePos, null);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            eraseable = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            eraseable = false;
+        }
+    }
+}
