d67b605675fed87ec35f5bd0c9e6c7599006a9a0
Caleb Biasco <biasc007@umn.edu>
2019-01-26 14:00:52 -0500

    Building up action system

---
+++ Assets/Scripts/Actionable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Actionable : MonoBehaviour
+{
+    public readonly ActionSet[] actionSets;
+
+    public HashSet<string> GetAttributes()
+    {
+        HashSet<string> attributes = new HashSet<string>();
+        foreach(ActionSet actionSet in actionSets)
+        {
+            attributes.UnionWith(actionSet.attributes);
+        }
+
+        return attributes;
+    }
+
+    public void PerformAction()
+    {
+        PerformAction(new HashSet<string>(), null);
+    }
+
+    public void PerformAction(HashSet<string> matchingIds, Actionable item)
+    {
+        
+    }
+}
