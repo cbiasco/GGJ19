fa77217ae764c55334538011b5e15411518602c2
Rajat Gupta <rajatg1@andrew.cmu.edu>
2019-01-26 22:11:05 -0800

    Added canvas prefab

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    
-    public float speed = 5f;
+    [SerializeField]
+    Transform feet;
 
     private Rigidbody2D m_rigidBody;
     private bool m_controlledByMouse = true;
+
+    public float speed = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +22,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Movement
-        float vert = Input.GetAxis("Vertical");
-        float hor  = Input.GetAxis("Horizontal");
-
-        m_rigidBody.velocity = new Vector2(hor, vert) * speed;
-
         // Determine what should be controlling the look direction
         m_controlledByMouse = m_controlledByMouse || Input.GetMouseButton(0) || Input.GetMouseButton(1) || Input.GetMouseButton(2);
         Vector3 lookDir = new Vector3(Input.GetAxis("LookX"), Input.GetAxis("LookY"), 0f);
@@ -40,6 +36,17 @@ public class PlayerController : MonoBehaviour
             Vector3 playerToCamera = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
             playerToCamera.z = 0;
             transform.up = playerToCamera;
+        }
+
+        // Movement
+        float vert = Input.GetAxis("Vertical");
+        float hor = Input.GetAxis("Horizontal");
+
+        m_rigidBody.velocity = new Vector2(hor, vert) * speed;
+
+        if (feet && m_rigidBody.velocity.sqrMagnitude > .01)
+        {
+            feet.up = m_rigidBody.velocity;
         }
     }
 }
